f4d466a717076e7ecf168c89d480b391d76e710f
Meqzouille <meqzeu@hotmail.com>
2026-04-30 05:25:26 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
+import discord
 
+TOKEN = "MTQ5OTIxNTI3MTIzMDA0NjI1OQ.G8TZ0R.Jek9Fbn9kAXthkVkJBpdyzRRKT22RMz7V0rZP8"
+
+SALON_LOGS_ID = 1498890074601951282
+SALON_RECAP_ID = 1499222076693676193
+RR_TRACKER_ID = 1220016509888364608
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Connecté en tant que {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author.id != RR_TRACKER_ID:
+        return
+
+    if message.channel.id != SALON_LOGS_ID:
+        return
+
+    if message.embeds:
+        embed = message.embeds[0]
+
+        if embed.title and "Récapitulatif de la veille" in embed.title:
+            channel = client.get_channel(SALON_RECAP_ID)
+            await channel.send(embed=embed)
+
+import os
+client.run(os.getenv("TOKEN"))
